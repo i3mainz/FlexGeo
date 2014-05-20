eb050ec6a1a1bc0cbc0138646218b8e088f030af
Nikolai Bock <nikolai.bock@geoinform.fh-mainz.de>
2014-05-20 13:47:37 +0000

    update TimeChart
    git-svn-id: https://svn.geoinform.fh-mainz.de/svn/i3_flexgeo/trunk/flexgeo@90 9598b5b8-6e75-4d1b-9c97-ac04647aac75

--- portal/liferay/portlets/TimeChartPortlet/src/main/java/de/i3mainz/flexgeo/portal/liferay/timechart/portlet/TimeChartPortlet.java
+++ portal/liferay/portlets/TimeChartPortlet/src/main/java/de/i3mainz/flexgeo/portal/liferay/timechart/portlet/TimeChartPortlet.java
@@ -1,12 +1,28 @@
 package de.i3mainz.flexgeo.portal.liferay.timechart.portlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
+import javax.portlet.PortletContext;
 import javax.portlet.PortletException;
 import javax.portlet.PortletPreferences;
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
+
+import net.opengis.om.x10.ObservationCollectionDocument;
+import net.opengis.om.x10.ObservationCollectionType;
+
+import org.apache.xmlbeans.XmlObject;
+import org.n52.oxf.OXFException;
+import org.n52.oxf.feature.OXFFeatureCollection;
+import org.n52.oxf.feature.OXFObservationCollectionType;
+import org.n52.oxf.ows.ExceptionReport;
+import org.n52.oxf.xmlbeans.parser.XMLBeansParser;
+import org.n52.oxf.xmlbeans.parser.XMLHandlingException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.portlet.context.PortletApplicationContextUtils;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
@@ -20,61 +36,110 @@ import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portlet.PortletPreferencesFactoryUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
+import de.i3mainz.springframework.swe.n52.sos.core.SOSTemplate;
+
 public class TimeChartPortlet extends MVCPortlet {
 
-	private static Log LOG = LogFactoryUtil.getLog(TimeChartPortlet.class);
+    private static Log LOG = LogFactoryUtil.getLog(TimeChartPortlet.class);
 
-	@Override
-	public void serveResource(ResourceRequest resourceRequest,
-			ResourceResponse resourceResponse) throws IOException,
-			PortletException {
+    @Override
+    public void serveResource(ResourceRequest resourceRequest,
+            ResourceResponse resourceResponse) throws IOException,
+            PortletException {
 
-		if ("loadChartData".equals(ParamUtil.getString(resourceRequest,
-				"getRequest"))) {
+        getContextBean();
 
-			String bbox = ParamUtil.getString(resourceRequest, "reqbbox", null);
-			ThemeDisplay themeDisplay = (ThemeDisplay) resourceRequest
-					.getAttribute(WebKeys.THEME_DISPLAY);
-			Object tmpObject = themeDisplay.getLayout().getExpandoBridge()
-					.getAttribute("reportID");
-			Long themeID;
-			if (tmpObject instanceof Long) {
-				themeID = (Long) tmpObject;
-			} else if (tmpObject instanceof Integer) {
-				themeID = new Long((Integer) tmpObject);
-			} else if (tmpObject instanceof String) {
-				themeID = Long.parseLong(tmpObject.toString());
-			} else {
-				themeID = new Long(0);
-			}
-			LOG.info("REPORTID: " + themeID);
-			LOG.info("BoundingBox from Request: " + bbox);
+        if ("loadChartData".equals(ParamUtil.getString(resourceRequest,
+                "getRequest"))) {
 
-			PortletPreferences portletPreferences = resourceRequest
-					.getPreferences();
-			String portletResource = ParamUtil.getString(resourceRequest,
-					"portletResource");
+            String bbox = ParamUtil.getString(resourceRequest, "reqbbox", null);
+            ThemeDisplay themeDisplay = (ThemeDisplay) resourceRequest
+                    .getAttribute(WebKeys.THEME_DISPLAY);
+            Object tmpObject = themeDisplay.getLayout().getExpandoBridge()
+                    .getAttribute("reportID");
+            Long themeID;
+            if (tmpObject instanceof Long) {
+                themeID = (Long) tmpObject;
+            } else if (tmpObject instanceof Integer) {
+                themeID = new Long((Integer) tmpObject);
+            } else if (tmpObject instanceof String) {
+                themeID = Long.parseLong(tmpObject.toString());
+            } else {
+                themeID = new Long(0);
+            }
+            LOG.info("REPORTID: " + themeID);
+            LOG.info("BoundingBox from Request: " + bbox);
 
-			if (Validator.isNotNull(portletResource)) {
-				try {
-					portletPreferences = PortletPreferencesFactoryUtil
-							.getPortletSetup(resourceRequest, portletResource);
-				} catch (PortalException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (SystemException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+            PortletPreferences portletPreferences = resourceRequest
+                    .getPreferences();
+            String portletResource = ParamUtil.getString(resourceRequest,
+                    "portletResource");
 
-			int precision = GetterUtil.getInteger(portletPreferences.getValue(
-					"aggregationprecision", String.valueOf(Calendar.HOUR_OF_DAY)));
+            if (Validator.isNotNull(portletResource)) {
+                try {
+                    portletPreferences = PortletPreferencesFactoryUtil
+                            .getPortletSetup(resourceRequest, portletResource);
+                } catch (PortalException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (SystemException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
 
-			ChartDataLoader chartData = new WFSChartDataLoader(themeID, bbox,
-					precision);
-			chartData.write(resourceResponse.getPortletOutputStream());
+            int precision = GetterUtil.getInteger(portletPreferences.getValue(
+                    "aggregationprecision",
+                    String.valueOf(Calendar.HOUR_OF_DAY)));
 
-		}
-	}
+            ChartDataLoader chartData = new WFSChartDataLoader(themeID, bbox,
+                    precision);
+            chartData.write(resourceResponse.getPortletOutputStream());
+
+        }
+    }
+
+    public void getContextBean() {
+
+        PortletContext pc = this.getPortletContext();
+        ApplicationContext context = PortletApplicationContextUtils
+                .getWebApplicationContext(pc);
+
+        // getting settings as object
+        SOSTemplate template = (SOSTemplate) context.getBean("sosReader");
+        List<String> observedProperties = new ArrayList<String>();
+        observedProperties.add("urn:ogc:def:dataType:OGC:1.1:string");
+        try {
+//            IFeatureStore featureStore = new SOSObservationStore(
+//                    template.getObservation("Twitter_Sentiment",
+//                            observedProperties));
+//            OXFFeatureCollection oxfFeatureCollection = featureStore
+//                    .unmarshalFeatures();
+//            
+//            LOG.info("Observation-SIZE: "+oxfFeatureCollection.size());
+//
+//            for (Iterator<OXFFeature> itr = oxfFeatureCollection.iterator(); itr
+//                    .hasNext();) {
+//                OXFFeature feature = itr.next();
+//                LOG.info("FeatureID: "+feature.getID());
+//            }
+            XmlObject xmlObject = XMLBeansParser.parse(template.getObservation("Twitter_Sentiment",
+                  observedProperties).getIncomingResultAsStream(), false);
+            ObservationCollectionDocument obsCollectionDoc = (ObservationCollectionDocument) xmlObject;
+            ObservationCollectionType observation = obsCollectionDoc.getObservationCollection();
+            LOG.info("Observation-ID: " +observation.getId());
+            OXFObservationCollectionType type = new OXFObservationCollectionType();
+            OXFFeatureCollection featureCollection = new OXFFeatureCollection(observation.getId(), type);
+            //LOG.info(oxfFeatureCollection.size());
+            
+            //LOG.info("XML-TEXT: "+xmlObject.xmlText());
+        } catch (OXFException e) {
+            LOG.error("Problem with OXF-Framework while SOS-Request", e);
+        } catch (ExceptionReport e) {
+            LOG.error("Error in SOS-Request", e);
+        } catch (XMLHandlingException e) {
+            LOG.error("XML-Handling failed", e);
+        }
+
+    }
 }
---
+++ portal/liferay/portlets/TimeChartPortlet/src/main/java/de/i3mainz/flexgeo/portal/liferay/timechart/test/MyBean.java
@@ -0,0 +1,44 @@
+package de.i3mainz.flexgeo.portal.liferay.timechart.test;
+
+public class MyBean {
+    private String name;
+    private int age;
+    private String course;
+    
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    /**
+     * @return the age
+     */
+    public final int getAge() {
+        return age;
+    }
+    /**
+     * @param age the age to set
+     */
+    public final void setAge(int age) {
+        this.age = age;
+    }
+    /**
+     * @return the course
+     */
+    public final String getCourse() {
+        return course;
+    }
+    /**
+     * @param course the course to set
+     */
+    public final void setCourse(String course) {
+        this.course = course;
+    }
+}
